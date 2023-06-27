ec5c072bdcefb00fe34715f877e75e20d9401b87
Leonardo Perdomo <leonardop763@gmail.com>
2023-06-27 14:48:32 -0400

    Feat (Skeleton): An skeleton is added while waiting the image to load, Fix (Dependecies): An npm clear cache is made

--- image-uploader-frontend/src/constants/images.js
+++ image-uploader-frontend/src/constants/images.js
@@ -1,7 +1,9 @@
 import success from '../assets/checked.png';
 import error from '../assets/cancel.png';
+import image from '../assets/image.svg';
 
 export const images = {
     success,
-    error
+    error,
+    image
 };
